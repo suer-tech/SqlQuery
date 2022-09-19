72cba7c1590e0ef76ecf35d2befd2bdee09f4264
Suer-Tech <102809506+suer-tech@users.noreply.github.com>
2022-09-19 16:35:34 +0500

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿SELECT [ProductId], COUNT(ProductId) as Times
+FROM [Market].[dbo].[Sales] sal 
+	INNER JOIN (
+		SELECT [CustomerId], MIN(DateCreated) as BecomeBuyerDate 
+		FROM [Market].[dbo].[Sales] GROUP BY CustomerId
+	) as cus 
+	ON sal.CustomerId = cus.CustomerId AND 
+	   sal.DateCreated = cus.BecomeBuyerDate
+GROUP BY ProductId
